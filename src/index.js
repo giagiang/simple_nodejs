a02aa542836223a44949c9a7b6f6d33dd24281b5
fastboy <giagiang200@gmail.com>
2023-12-29 14:46:30 +0700

    config routes in express js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars  =  require( 'express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public'  )));
 app.use(express.urlencoded({
   extended: true
@@ -15,9 +17,6 @@ app.use(express.json());
 app.use(morgan('combined'));
 
 // neu goi ntn thanh cong, nghia la handlebars se la 1 function.
-// function_name()
-
-// const { engine } = handlebars
 //Template engine
 app.engine('hbs', handlebars.engine({
   extname: '.hbs'
@@ -26,27 +25,9 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// console.log(__dirname)
+//Route init
+route(app);
 
-app.get("/", (req, res) => {
-  res.render('home')
-});
-
-app.get("/news", (req, res) => {
-  console.log(req.query.q)
-  res.render('news')
-});
-
-app.get("/search", (req, res) => {
-  res.render('search')
-});
-app.post("/search",(req,res)=>{
-  console.log(req.body)
-  res.send('')
-})
-app.post("/profile",(req,res)=>{
-  res.render('profile')
-})
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
