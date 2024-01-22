8bcaf0362f96452266eaa7b26c8727f5d0b94ce5
fastboy <giagiang200@gmail.com>
2024-01-22 23:37:22 +0700

    deleted count in trash

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,7 +3,13 @@ import Course from "../models/Course.js";
 class MeController {
   //[Get], /me/stored/courses
   storedCourses(req, res, next) {
-    Course.find({deletedAt: {$exists: false}})
+    Course.countDocuments({deleted:true})
+          .then((deletedCount) => {
+        console.log(deletedCount);
+      })
+      .catch(()=>{});
+
+    Course.find({ deletedAt: { $exists: false } })
       .lean()
       .then((courses) =>
         res.render("me/stored-courses", {
@@ -12,17 +18,17 @@ class MeController {
       )
       .catch(next);
   }
-  
-  trashCourses(req, res, next) {
-    Course.find({deletedAt: {$exists: true}})
-    .lean()
-      .then((courses) => {
 
+  //[Get] /me/stored/courses
+  trashCourses(req, res, next) {
+    Course.find({ deletedAt: { $exists: true } })
+      .lean()
+      .then((courses) => {
         res.render("me/trash-courses", {
           courses,
         });
       })
       .catch(next);
-}
+  }
 }
 export default new MeController();
