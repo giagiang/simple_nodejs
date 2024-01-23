2bfc6430d2f17ffbafd17512a9dee3561095942a
fastboy <giagiang200@gmail.com>
2024-01-23 10:29:22 +0700

    fix bug deleted count document

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,21 +3,33 @@ import Course from "../models/Course.js";
 class MeController {
   //[Get], /me/stored/courses
   storedCourses(req, res, next) {
-    Course.countDocuments({deleted:true})
-          .then((deletedCount) => {
-        console.log(deletedCount);
-      })
-      .catch(()=>{});
-
-    Course.find({ deletedAt: { $exists: false } })
-      .lean()
-      .then((courses) =>
+    Promise.all([
+      Course.find({ deletedAt: { $exists: false } }).lean(),
+      Course.countDocuments({deleted:true}),
+    ])
+      .then(([courses, deletedCount]) =>
         res.render("me/stored-courses", {
+          deletedCount,
           courses,
         })
       )
       .catch(next);
   }
+  //   Course.countDocuments({deleted:true})
+  //         .then((deletedCount) => {
+  //       console.log(deletedCount);
+  //     })
+  //     .catch(()=>{});
+
+  //   Course.find({ deletedAt: { $exists: false } })
+  //     .lean()
+  //     .then((courses) =>
+  //       res.render("me/stored-courses", {
+  //         courses,
+  //       })
+  //     )
+  //     .catch(next);
+  // }
 
   //[Get] /me/stored/courses
   trashCourses(req, res, next) {
