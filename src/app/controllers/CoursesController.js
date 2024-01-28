e346368f3f1d6609f2c0cc3f4f3503fb67d80803
fastboy <giagiang200@gmail.com>
2024-01-28 13:53:57 +0700

    update select checkbox

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -20,12 +20,11 @@ class CoursesController {
   //[POST] /courses/store
 
   store(req, res, next) {
-
-    req.body.image  = `https://i.ytimg.com/vi/${req.body.videoId}/maxresdefault.jpg`;
+    req.body.image = `https://i.ytimg.com/vi/${req.body.videoId}/maxresdefault.jpg`;
     const course = new Course(req.body);
     course
       .save()
-      .then(() => res.redirect('/me/stored/courses  '))
+      .then(() => res.redirect("/me/stored/courses  "))
       .catch((error) => {});
   }
   //[GET]/COURSES/:id/:edit
@@ -54,10 +53,22 @@ class CoursesController {
       .catch(next);
   }
   //[DELETE]/course/:id/force
-  forceDelete(req,res,next){
-      Course.deleteOne({_id: req.params.id})
-      .then(()=>res.redirect('back'))
+  forceDelete(req, res, next) {
+    Course.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect("back"))
       .catch(next);
+  }
+  //[POST]/COURSE/handle-form-actions
+  handleFormActions(req, res, next) {
+    switch (req.body.action) {
+      case "delete":
+        Course.delete({ _id: { $in: req.body.courseIds } })
+          .then(() => res.redirect("back"))
+          .catch(next);
+        break;
+      default:
+        res.json({ message: "action is invalid" });
+    }
   }
 }
 
